0bfd4aa6e05e2b31fd73eba233ae0b1a6268e3b2
Ouma <dennisouma96@gmail.com>
2024-09-17 09:42:11 +0300

    Ready for deployment

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 
+if(process.env.NODE_ENV === 'production') disableReactDevTools()
+  
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
