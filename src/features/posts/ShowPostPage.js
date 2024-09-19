7d629a7ab9883115ed926943d6806eb96292bc97
DENIS_OUMA_OTIENO <48428763+DENIS-OUMA-OTIENO@users.noreply.github.com>
2024-09-19 09:47:56 +0300

    Update ShowPostPage.js

--- src/features/posts/ShowPostPage.js
+++ src/features/posts/ShowPostPage.js
@@ -4,6 +4,9 @@ import PostPage from './PostPage'
 import Box from '@mui/material/Box'
 import Skeleton from '@mui/material/Skeleton'
 import { useParams } from 'react-router-dom'
+import Footer from '../../components/Footer'
+import Header from '../../components/Header'
+
 
 const ShowPostPage = () => {
     // const postId = useSelector(state => state.post.postId)
@@ -20,12 +23,16 @@ const ShowPostPage = () => {
     let content
     if(!post){ 
         content = (
+        <>
+        <Header sections={sections} />
         <Box>
             <Skeleton variant="rectangular" height={300} /> 
             <Skeleton variant="text" height={40} width="80%" /> 
             <Skeleton variant="text" height={20} width="100%" /> 
             <Skeleton variant="text" height={20} width="100%" /> 
         </Box>)
+        <Footer />
+        </>
     } else {
     content = <PostPage post={post}/>
     }
